13615ed45a224d60607e6299b6aaf6f1276ff8b9
Rafael Marques <rafael@RAFAEL-PC>
2023-03-14 22:00:26 -0300

    commits

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -69,6 +69,7 @@ class NotesController {
         .whereLike("notes.title", `%${title}%`)
         .whereIn("name", filterTags)
         .innerJoin("notes", "notes.id", "tags.note_id")
+        .groupBy("notes.id")
         .orderBy("notes.title")
     } else {
       notes = await knex("notes")
@@ -81,7 +82,7 @@ class NotesController {
     const notesWithTag = notes.map((note) => {
       const noteTags = userTags.filter((tag) => tag.note_id === note.id)
       return {
-        ...notes,
+        ...note,
         tags: noteTags,
       }
     })
