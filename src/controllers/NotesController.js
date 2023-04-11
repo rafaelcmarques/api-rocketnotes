ca07b6df624b9bea531ecfb712bb49526528c362
Rafael Marques <rafael@RAFAEL-PC>
2023-04-11 01:36:35 -0300

    alteração no create

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -5,7 +5,7 @@ class NotesController {
     const { title, description, tags, links } = request.body
     const user_id = request.user.id
 
-    const notes_id = await knex("notes").insert({
+    const [note_id] = await knex("notes").insert({
       title,
       description,
       user_id,
@@ -13,7 +13,7 @@ class NotesController {
 
     const linksInsert = links.map((link) => {
       return {
-        note_id: notes_id,
+        note_id,
         url: link,
       }
     })
@@ -22,7 +22,7 @@ class NotesController {
 
     const tagsInsert = tags.map((name) => {
       return {
-        note_id: notes_id,
+        note_id,
         name,
         user_id,
       }
